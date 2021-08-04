e410575a2276eb39d13c0007867b87332286d762
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-08-04 11:01:19 +0300

    creating details for an image

--- images/views.py
+++ images/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
 
 
 @login_required
@@ -11,7 +13,6 @@ def image_create(request):
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
             # form data is valid
-            cd = form.cleaned_data
             new_item = form.save(commit=False)
             # assign current user to the item
             new_item.user = request.user
@@ -26,3 +27,11 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
     return render(request, 'images/image/create.html', {'section': 'images',
                                                         'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
