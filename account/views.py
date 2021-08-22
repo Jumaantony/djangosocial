b21378a39e75a66fc34d5242bc038a77d6f59289
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-08-22 14:57:31 +0300

    tweaked error in views

--- account/views.py
+++ account/views.py
@@ -94,7 +94,7 @@ def edit(request):
 
 @login_required
 def user_list(request):
-    users = User.ojects.filter(is_active=True)
+    users = User.objects.filter(is_active=True)
     return render(request, 'account/user/list.html',
                   {'section': 'people',
                    'users': users})
