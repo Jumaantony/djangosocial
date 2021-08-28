4d4c8ccbbd154324ca13b03e45c7f4bdee31851f
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2021-08-28 08:45:54 +0300

    tweaking redis configuration error

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -191,7 +191,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Redis Configuration
-REDIS_HOST = 'localhost'
+REDIS_HOST = 'https://socialdjango2.herokuapp.com'
 REDIS_PORT = 6379
 REDIS_DB = 0
 
